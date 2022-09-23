086ce9e3d1cac023da6ae9047d20eb5a4b6690b1
caH40 <kmvsancho@gmail.com>
2022-09-23 18:51:58 +0300

    revision of the setup module

--- app_modules/setup.js
+++ app_modules/setup.js
@@ -2,21 +2,19 @@ import { BotSetup } from '../model/BotSetup.js';
 import { adminVerify } from './admin-verify.js';
 
 //настройка бота для канала, данные сохраняются в БД
-//урл на API погоды
-//id и название канала
-//id и название дискуссионной группы канала
 //определиться где добавляются города, из общего чата или из сессии сетап
 export async function setup(ctx) {
 	try {
+		const botSetupDB = await BotSetup.findOne();
+
+		const chatName = botSetupDB ? botSetupDB.groupTitle : '';
 		// проверка должна осуществляться из данных DB
 		const isAdmin = await adminVerify(ctx);
-		if (!isAdmin)
-			return await ctx.reply('Команда доступна только админам канала "channelName from DB "');
+		if (!isAdmin) return await ctx.reply(`Команда доступна админам и только из группы ${chatName}`);
 
 		const userId = ctx.message.from.id;
 		// Если коллекция уже есть, то больше не создается. Может быть только одна коллекция BotSetup.
-		const response = await BotSetup.findOne();
-		if (!response) {
+		if (!botSetupDB) {
 			console.log('создаю документ botSetup');
 			const newBotSetup = new BotSetup({ channelOwnerId: userId });
 			await newBotSetup.save();
--- model/Meetup.js
+++ model/Meetup.js
@@ -4,7 +4,7 @@ import pkg from 'mongoose';
 const { Schema, model } = pkg;
 
 const meetupSchema = new Schema({
-	botId: { type: mongoose.Schema.Types.ObjectId, ref: 'BotSetupUser' },
+	botId: { type: mongoose.Schema.Types.ObjectId, ref: 'BotSetup' },
 	postedBy: { type: String },
 	city: { type: String },
 	cityWeather: { type: String },
