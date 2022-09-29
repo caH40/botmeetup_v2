8debe54680b60e37efa9e5541ea8ea1d62bb36bd
caH40 <kmvsancho@gmail.com>
2022-09-29 21:06:39 +0300

    added the example object location with a weather

---
+++ weather/location-weather.js
@@ -0,0 +1,7 @@
+// list of cities with coordinates
+export const locationWeatherList = [
+	{
+		locationName: 'Кисловодск',
+		LocationWeather: ['Кисловодск', 'Барашек', 'Пятигорск', 'Ессентуки', 'Суворовская'],
+	},
+];
