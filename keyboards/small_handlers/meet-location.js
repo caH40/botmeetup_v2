99c973977163f9df00981fd6e8d0ba85ccc614c4
caH40 <kmvsancho@gmail.com>
2022-10-02 18:17:07 +0300

    added a description when the error -no starting places- occurs

--- keyboards/small_handlers/meet-location.js
+++ keyboards/small_handlers/meet-location.js
@@ -7,5 +7,9 @@ export async function meetLocations(ctx, cbqData) {
 	ctx.session.locationWeather = '';
 	ctx.session.start[2][1].text = 'Погода';
 	const locationsDB = await Location.find();
+	if (locationsDB.length == 0)
+		return ctx.reply(
+			'Нет данных! Первоначально необходимо выполнить настройку бота, добавив "места старта" командой /location. Затем, если необходимо, добавить "места погоды" каждому "месту старта" для мониторинга погоды /weather'
+		);
 	getKeyboard(ctx, 'Место старта', keyboardMainLocations(locationsDB));
 }
