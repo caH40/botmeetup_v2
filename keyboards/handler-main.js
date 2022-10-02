06e0615c108cd5ede2ae2e1b7564af31cc2dc572
caH40 <kmvsancho@gmail.com>
2022-10-02 08:18:29 +0300

    minor fixes

--- keyboards/handler-main.js
+++ keyboards/handler-main.js
@@ -20,42 +20,37 @@ export async function handlerMainMenu(ctx, cbqData) {
 	try {
 		console.log(cbqData);
 		// меню время
-		if (cbqData === 'meetTime') {
-			getKeyboard(ctx, 'Время старта', keyboardMeetingTimes);
-		}
+		if (cbqData === 'meetTime') await getKeyboard(ctx, 'Время старта', keyboardMeetingTimes);
+
 		// меню с датами выбираем
-		if (cbqData === 'meetDate') {
-			getKeyboard(ctx, 'Дата запланированного заезда', getKeyboardDays());
-		}
+		if (cbqData === 'meetDate')
+			await getKeyboard(ctx, 'Дата запланированного заезда', getKeyboardDays());
+
 		// меню места
 		if (cbqData === 'meetLocation') {
 			const locationsDB = await Location.find();
 			getKeyboard(ctx, 'Место старта', keyboardMainLocations(locationsDB));
 		}
 		// меню дистанций
-		if (cbqData === 'meetDistance') {
-			getKeyboard(ctx, 'Дистанция заезда', keyboardDistances);
-		}
+		if (cbqData === 'meetDistance') await getKeyboard(ctx, 'Дистанция заезда', keyboardDistances);
+
 		// меню скорости
-		if (cbqData === 'meetSpeed') {
-			getKeyboard(ctx, 'Средняя скорость заезда, км/ч', keyboardSpeed);
-		}
+		if (cbqData === 'meetSpeed')
+			await getKeyboard(ctx, 'Средняя скорость заезда, км/ч', keyboardSpeed);
+
 		// меню погода
-		if (cbqData === 'meetWeather') {
-			await meetWeather(ctx, cbqData);
-		}
+		if (cbqData === 'meetWeather') await meetWeather(ctx, cbqData);
+
 		// // меню сложности
 		// if (cbqData === 'meetLevel') {
 		// 	getKeyboard(ctx, 'Уровень сложности заезда', keyboardDifficulty);
 		// }
 		// меню загрузки картинки
-		if (cbqData === 'meetCover') {
-			await ctx.scene.enter('getPhoto');
-		}
+		if (cbqData === 'meetCover') await ctx.scene.enter('getPhoto');
+
 		// меню описания заезда
-		if (cbqData === 'meetDescription') {
-			await ctx.scene.enter('getDescription');
-		}
+		if (cbqData === 'meetDescription') await ctx.scene.enter('getDescription');
+
 		// вывод меню сводных данных по заезду, публикация или редактирование
 		if (cbqData === 'meetSummary') {
 			if (ctx.session.photoId) {
