245f97652b51814cb1d2befb4f99cb3909b7905e
caH40 <kmvsancho@gmail.com>
2022-10-03 11:10:00 +0300

    minor fixes

--- controllers/start.js
+++ controllers/start.js
@@ -1,10 +1,12 @@
 import { startMessage } from '../app_modules/texts.js';
+import { BotSetup } from '../model/BotSetup.js';
 
 export async function start(ctx) {
 	try {
+		const { channelName } = await BotSetup.findOne();
 		const userName = ctx.message.from.username;
 
-		ctx.reply(`Привет ${userName ? userName : 'незнакомец'} ! ${startMessage()}`, {
+		ctx.reply(`Привет ${userName ? userName : 'незнакомец'} ! ${startMessage(channelName)}`, {
 			parse_mode: 'html',
 			disable_web_page_preview: true,
 		});
