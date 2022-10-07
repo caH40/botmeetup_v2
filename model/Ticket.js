2dde9a0f9bed251e60449d5e6049a2e16c7cb816
caH40 <kmvsancho@gmail.com>
2022-10-07 15:30:53 +0300

    changed the model Ticket

--- model/Ticket.js
+++ model/Ticket.js
@@ -3,11 +3,11 @@ import pkg from 'mongoose';
 
 const { Schema, model } = pkg;
 
-const locationSchema = new Schema({
+const ticketSchema = new Schema({
 	ownerId: { type: Number },
 	datePurchase: { type: Number },
 	duration: { type: Number },
 	isActive: { type: Boolean, default: false },
 });
 
-export const Location = model('Location', locationSchema);
+export const Ticket = model('Ticket', ticketSchema);
