37232efd9b4f146a82033cc58c744c8fbd82c989
caH40 <kmvsancho@gmail.com>
2022-10-09 10:00:15 +0300

    changed the module help

--- controllers/help.js
+++ controllers/help.js
@@ -1,7 +1,11 @@
+import { chatsMember } from '../app_modules/chat-member.js';
 import { commandsMessage } from '../app_modules/texts.js';
 
 export async function help(ctx) {
 	try {
+		const isMember = await chatsMember(ctx);
+		if (!isMember) return;
+
 		await ctx.reply(commandsMessage);
 	} catch (error) {
 		console.log(error);
