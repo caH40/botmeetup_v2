187e43880407eee4d738c6a358fe3f6cb2b365cf
caH40 <kmvsancho@gmail.com>
2022-10-09 10:32:53 +0300

    minor fixes in the module start

--- controllers/start.js
+++ controllers/start.js
@@ -1,19 +1,20 @@
 import { chatsMember } from '../app_modules/chat-member.js';
 import { startMessage } from '../app_modules/texts.js';
-import { BotSetup } from '../model/BotSetup.js';
 
 export async function start(ctx) {
 	try {
 		const isMember = await chatsMember(ctx);
 		if (!isMember) return;
 
-		const { channelName } = await BotSetup.findOne({ channelId: ctx.session.channelId });
 		const userName = ctx.message.from.username;
 
-		ctx.reply(`Привет ${userName ? userName : 'незнакомец'} ! ${startMessage(channelName)}`, {
-			parse_mode: 'html',
-			disable_web_page_preview: true,
-		});
+		ctx.reply(
+			`Привет ${userName ? userName : 'незнакомец'} ! ${startMessage(ctx.session.channelName)}`,
+			{
+				parse_mode: 'html',
+				disable_web_page_preview: true,
+			}
+		);
 	} catch (error) {
 		console.log(error);
 	}
