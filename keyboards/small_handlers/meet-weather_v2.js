36f3ac5cfb6aab7bb3815e4d237678b293b54adc
Alexander Berezhnev <freelancerbav@gmail.com>
2023-06-21 13:04:55 +0300

    minor fixes

--- keyboards/small_handlers/meet-weather_v2.js
+++ keyboards/small_handlers/meet-weather_v2.js
@@ -3,35 +3,35 @@ import { getKeyboard } from '../keyboard-get.js';
 import { keyboardLocation } from '../keyboards.js';
 
 export async function meetWeather_v2(ctx) {
-	try {
-		const locationStart = ctx.session.locationStart;
-		const text = ctx.message.text;
-		// обнуление значение погоды в сессии
-		ctx.session.locationWeather = '---';
-		ctx.session.start[1][1].text = 'Погода';
+  try {
+    const locationStart = ctx.session.locationStart;
+    const text = ctx.message.text;
+    // обнуление значение погоды в сессии
+    ctx.session.locationWeather = '---';
+    ctx.session.start[1][1].text = 'Погода';
 
-		const cities = [];
-		const citiesDB = await City.find({ name: { $regex: text } });
+    const cities = [];
+    const citiesDB = await City.find({ name: { $regex: text, $options: 'i' } });
 
-		for (let index = 0; index < citiesDB.length; index++) {
-			cities.push(citiesDB[index]);
-		}
+    for (let index = 0; index < citiesDB.length; index++) {
+      cities.push(citiesDB[index]);
+    }
 
-		if (cities.length > 20)
-			return await ctx.reply(
-				'Нашлось слишком много вариантов, сузьте поиск города, увеличьте количество букв. Для выхода /quit'
-			);
-		if (cities.length === 0)
-			return await ctx.reply(
-				`Ничего не нашлось.\nВвод необходимо осуществлять на латинице начиная с заглавной буквы. Для выхода /quit`
-			);
+    if (cities.length > 20)
+      return await ctx.reply(
+        'Нашлось слишком много вариантов, сузьте поиск города, увеличьте количество букв. Для выхода /quit'
+      );
+    if (cities.length === 0)
+      return await ctx.reply(
+        `Ничего не нашлось.\nВвод необходимо осуществлять на латинице начиная с заглавной буквы. Для выхода /quit`
+      );
 
-		getKeyboard(
-			ctx,
-			`Выберите нужный город для наблюдения за погодой при старте из <b>${locationStart}</b>`,
-			keyboardLocation(cities, 'weather_')
-		);
-	} catch (error) {
-		console.log(error);
-	}
+    getKeyboard(
+      ctx,
+      `Выберите нужный город для наблюдения за погодой при старте из <b>${locationStart}</b>`,
+      keyboardLocation(cities, 'weather_')
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
